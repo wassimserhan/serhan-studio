fad7d850582a69558283eef2dbaee4322e183583
Wassim Serhan <wserhan@gmail.com>
2025-01-28 18:03:49 -0500

    edits

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Serhan Studio</title>
+        <title>Lake Street Studio</title>
         <meta name="description" content="WordPress & Shopify" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.png" />
